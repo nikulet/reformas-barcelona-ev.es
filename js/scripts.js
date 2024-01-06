eac27b0ab7b47c2d3382024a416ad7aec990db68
Nicu Ceban <nikulet01@gmail.com>
2024-01-06 14:08:20 +0100

    sdf

--- js/scripts.js
+++ js/scripts.js
@@ -154,7 +154,7 @@ function update() {
 }
 
 var currentURL = window.location.href;
-    if (currentURL.includes("index.html")) {
+    if (currentURL.includes("")) {
 location.href = 'https://reformas-barcelona-ev.es/index.html';  /* en caso si la url es www.reformas-barcelona-ev.es (es null) se va a
 anadir www.reformas-barcelona-eval.es/index.html/  */
     }
